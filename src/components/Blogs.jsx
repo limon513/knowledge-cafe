e171ddd4a6936d0f5ec83c36eec8666a83dd40b9
limon513 <sabur.islam@sec.ac.bd>
2024-11-18 18:47:45 +0600

    Test commit for some problems

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import Blog from "./Blog";
 
 export default function Blogs() {
+  const [blogs, setBlogs] = useState([]);
+
   return (
     <div>
       <Blog />
